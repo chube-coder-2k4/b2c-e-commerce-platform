3f1bcf57f1e3798d500c566656db29219bd98b25
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2025-12-14 17:05:25 +0700

    feat: noti dto

---
+++ commerce-gateway-development/src/main/java/dev/commerce/dtos/response/NotificationSocketResponse.java
@@ -0,0 +1,18 @@
+package dev.commerce.dtos.response;
+
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+public class NotificationSocketResponse {
+    private UUID id;
+    private String title;
+    private String message;
+    private boolean read;
+    private String type;
+    private String referenceId;
+    private Instant createdAt;
+}
+
