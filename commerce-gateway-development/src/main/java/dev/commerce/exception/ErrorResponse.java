b0212e98cdba72270b0a46f77ad785b4b4f7af48
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2025-12-14 15:50:42 +0700

    feat: handle exception

--- commerce-gateway-development/src/main/java/dev/commerce/exception/ErrorResponse.java
+++ commerce-gateway-development/src/main/java/dev/commerce/exception/ErrorResponse.java
@@ -1,5 +1,6 @@
 package dev.commerce.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class ErrorResponse implements Serializable {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
     private int status;
     private String path;
--- commerce-gateway-development/src/main/java/dev/commerce/exception/GlobalHandlerException.java
+++ commerce-gateway-development/src/main/java/dev/commerce/exception/GlobalHandlerException.java
@@ -1,12 +1,50 @@
 package dev.commerce.exception;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class GlobalHandlerException {
+
+    ErrorResponse e = new ErrorResponse();
+
     @ExceptionHandler(Exception.class)
-    public String handleAllExceptions(Exception ex) {
-        return "An error occurred: " + ex.getMessage();
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleAllExceptions(Exception ex, WebRequest request) {
+        e.setMessage(ex.getMessage());
+        e.setTimestamp(LocalDateTime.now());
+        e.setStatus(INTERNAL_SERVER_ERROR.value());
+        e.setError(INTERNAL_SERVER_ERROR.getReasonPhrase());
+        e.setPath(request.getContextPath());
+        return e;
     }
+
+    @ExceptionHandler({ResourceNotFoundException.class, UserNotFoundException.class})
+    @ResponseStatus(NOT_FOUND)
+    public ErrorResponse handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        e.setMessage(ex.getMessage());
+        e.setTimestamp(LocalDateTime.now());
+        e.setStatus(NOT_FOUND.value());
+        e.setPath(request.getContextPath());
+        e.setError(NOT_FOUND.getReasonPhrase());
+        return e;
+    }
+
+    @ExceptionHandler(InvalidDataException.class)
+    @ResponseStatus(CONFLICT)
+    public ErrorResponse handleInvalidDataException(InvalidDataException ex, WebRequest request) {
+        e.setMessage(ex.getMessage());
+        e.setTimestamp(LocalDateTime.now());
+        e.setStatus(CONFLICT.value());
+        e.setPath(request.getContextPath());
+        e.setError(CONFLICT.getReasonPhrase());
+        return e;
+    }
+
 }
