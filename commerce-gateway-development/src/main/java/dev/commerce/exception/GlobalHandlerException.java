c4ab0813e5a5a0aa99180749ef37992d78879cea
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2026-01-06 21:24:10 +0700

    feat: handle exception

--- commerce-gateway-development/src/main/java/dev/commerce/exception/GlobalHandlerException.java
+++ commerce-gateway-development/src/main/java/dev/commerce/exception/GlobalHandlerException.java
@@ -1,50 +1,85 @@
 package dev.commerce.exception;
 
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
-
 import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class GlobalHandlerException {
 
-    ErrorResponse e = new ErrorResponse();
+    @ExceptionHandler({ConstraintViolationException.class,
+            MissingServletRequestParameterException.class, MethodArgumentNotValidException.class})
+    @ResponseStatus(BAD_REQUEST)
+    public ErrorResponse handleValidationException(Exception e, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setTimestamp(LocalDateTime.now());
+        errorResponse.setStatus(BAD_REQUEST.value());
+        errorResponse.setPath(request.getDescription(false).replace("uri=", ""));
 
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleAllExceptions(Exception ex, WebRequest request) {
-        e.setMessage(ex.getMessage());
-        e.setTimestamp(LocalDateTime.now());
-        e.setStatus(INTERNAL_SERVER_ERROR.value());
-        e.setError(INTERNAL_SERVER_ERROR.getReasonPhrase());
-        e.setPath(request.getContextPath());
-        return e;
+        String message = e.getMessage();
+        if (e instanceof MethodArgumentNotValidException) {
+            int start = message.lastIndexOf("[") + 1;
+            int end = message.lastIndexOf("]") - 1;
+            message = message.substring(start, end);
+            errorResponse.setError("Invalid Payload");
+            errorResponse.setMessage(message);
+        } else if (e instanceof MissingServletRequestParameterException) {
+            errorResponse.setError("Invalid Parameter");
+            errorResponse.setMessage(message);
+        } else if (e instanceof ConstraintViolationException) {
+            errorResponse.setError("Invalid Parameter");
+            errorResponse.setMessage(message.substring(message.indexOf(" ") + 1));
+        } else {
+            errorResponse.setError("Invalid Data");
+            errorResponse.setMessage(message);
+        }
+
+        return errorResponse;
     }
 
-    @ExceptionHandler({ResourceNotFoundException.class, UserNotFoundException.class})
+    @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(NOT_FOUND)
-    public ErrorResponse handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        e.setMessage(ex.getMessage());
-        e.setTimestamp(LocalDateTime.now());
-        e.setStatus(NOT_FOUND.value());
-        e.setPath(request.getContextPath());
-        e.setError(NOT_FOUND.getReasonPhrase());
-        return e;
+    public ErrorResponse handleResourceNotFoundException(ResourceNotFoundException e, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setTimestamp(LocalDateTime.now());
+        errorResponse.setPath(request.getDescription(false).replace("uri=", ""));
+        errorResponse.setStatus(NOT_FOUND.value());
+        errorResponse.setError(NOT_FOUND.getReasonPhrase());
+        errorResponse.setMessage(e.getMessage());
+
+        return errorResponse;
     }
 
     @ExceptionHandler(InvalidDataException.class)
     @ResponseStatus(CONFLICT)
-    public ErrorResponse handleInvalidDataException(InvalidDataException ex, WebRequest request) {
-        e.setMessage(ex.getMessage());
-        e.setTimestamp(LocalDateTime.now());
-        e.setStatus(CONFLICT.value());
-        e.setPath(request.getContextPath());
-        e.setError(CONFLICT.getReasonPhrase());
-        return e;
+    public ErrorResponse handleDuplicateKeyException(InvalidDataException e, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setTimestamp(LocalDateTime.now());
+        errorResponse.setPath(request.getDescription(false).replace("uri=", ""));
+        errorResponse.setStatus(CONFLICT.value());
+        errorResponse.setError(CONFLICT.getReasonPhrase());
+        errorResponse.setMessage(e.getMessage());
+
+        return errorResponse;
     }
 
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception e, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setTimestamp(LocalDateTime.now());
+        errorResponse.setPath(request.getDescription(false).replace("uri=", ""));
+        errorResponse.setStatus(INTERNAL_SERVER_ERROR.value());
+        errorResponse.setError(INTERNAL_SERVER_ERROR.getReasonPhrase());
+        errorResponse.setMessage(e.getMessage());
+
+        return errorResponse;
+    }
 }
